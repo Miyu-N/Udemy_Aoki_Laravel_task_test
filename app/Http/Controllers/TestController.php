a15b364e0380e2c259141b1f95f6e9caab867f64
Miyu-N <myu.star1217@gmail.com>
2022-02-20 20:13:26 +0900

    6-83 MVCモデルの記述方法

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test;
+
 class TestController extends Controller
 {
     //
+    public function index()
+    {
+
+        $values = Test::all();//全部のデータ
+
+        //dd($values);//変数の中身を表示
+
+        return view('tests.test', compact('values'));//渡す変数は''で囲む
+    }
 }
